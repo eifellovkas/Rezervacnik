b564cfed6f30bc4190b80fb834844253f1207d3d
havlikmar <havlikmar@gmail.com>
2018-04-21 09:43:09 +0200

    úprava - pridani casti restaurace

--- Rezervacnik/src/main/java/com/github/eifellovkas/Rezervacnik/logika/Restaurace.java
+++ Rezervacnik/src/main/java/com/github/eifellovkas/Rezervacnik/logika/Restaurace.java
@@ -1,7 +1,11 @@
 package com.github.eifellovkas.Rezervacnik.logika;
 
+import java.util.HashMap;
+
 public class Restaurace {
 	private int pocetStolu = 0;
+//	private HashMap<String,Stul> seznamStolu;
+//	private HashMap<String, Rezervace> seznamRezervaci;
 
 		public Restaurace() {
 		}
@@ -9,4 +13,21 @@ public class Restaurace {
 		public void pokus() {
 			System.out.println("aaaa");
 		}
+		
+		public int getPocetStolu() {
+			return pocetStolu;
+		}
+		
+		public void setPocetStolu(int pocetStolu) {
+			this.pocetStolu = pocetStolu;
+		}
+		
+//		public void pridejStul(String nazev, Stul stul) {
+//			seznamStolu.put(nazev, stul);
+//		}
+		
+//		public void odeberStul(String nazev) {
+//			seznamStolu.remove(nazev, stul);
+//		}
+
 }
