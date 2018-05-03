0225163e3ef1c03554f1a8d088a0757291fe4068
plesk <pleskacv@gmail.com>
2018-05-03 23:27:23 +0200

    Přidána třída Rezervace

---
+++ Rezervacnik/src/main/java/com/github/eifellovkas/Rezervacnik/logika/Stul.java
@@ -0,0 +1,28 @@
+package com.github.eifellovkas.Rezervacnik.logika;
+
+public class Stul {
+
+	private int pocetMist;
+	private boolean nekuracky = false;
+	
+	public Stul (int pocetMist, boolean nekuracky) {
+	this.pocetMist = pocetMist;
+	this.nekuracky = nekuracky;
+	}
+	
+	public int getPocetMist() {
+		return pocetMist;
+	}
+	
+	public void setPocetMist(int pocetMist) {
+		this.pocetMist = pocetMist;
+	}
+	
+	public boolean isNekuracky() {
+		return false;
+	}
+	
+	public void setNekuracky(boolean nekuracky) {
+		this.nekuracky = nekuracky;
+	}
+}
