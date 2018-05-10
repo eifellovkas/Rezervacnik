c372e5bb8b39735f62c085204b583727f299ee56
plesk <pleskacv@gmail.com>
2018-05-10 15:40:03 +0200

    ..

---
+++ Rezervacnik/src/main/java/com/github/eifellovkas/Rezervacnik/ui/ControllerSpravaRezervaci.java
@@ -0,0 +1,22 @@
+/**
+
+ * 
+ */
+package com.github.eifellovkas.Rezervacnik.ui;
+import com.github.eifellovkas.Rezervacnik.logika.*;
+/**
+ * @author plev00
+ *
+ */
+public class ControllerSpravaRezervaci {
+	private Rezervace rezervace;
+	private String nazev;
+
+	public ControllerSpravaRezervaci(Rezervace rezervace,String nazev) {
+		this.rezervace=rezervace;
+		this.nazev=nazev;
+		
+		
+	}
+	
+}
