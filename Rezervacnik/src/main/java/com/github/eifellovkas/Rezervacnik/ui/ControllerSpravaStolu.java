968b731040d820ebb7d84da699662c8698e983d4
AnetaBukovjanova <anetbukovjanova@gmail.com>
2018-05-10 16:03:41 +0200

    úprava controlleru sprava stolu a uprava fxml sprava stolu

--- Rezervacnik/src/main/java/com/github/eifellovkas/Rezervacnik/ui/ControllerSpravaStolu.java
+++ Rezervacnik/src/main/java/com/github/eifellovkas/Rezervacnik/ui/ControllerSpravaStolu.java
@@ -1,5 +1,25 @@
 package com.github.eifellovkas.Rezervacnik.ui;
 
-public class ControllerSpravaStolu {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import com.github.eifellovkas.Rezervacnik.logika.Stul;
 
+public class ControllerSpravaStolu extends GridPane{
+	private Stul stul;
+	
+	@FXML private ListView<String> 	seznamstolu;
+	@FXML private Button 			upravit;
+	@FXML private Button 			odstranit;
+	@FXML private ComboBox<Integer> pocetmist;
+	@FXML private CheckBox 			kuracky;
+	
+	
+	
+public void inicializuj(Stul stul) {
+
+}
 }
