2c416a3ae2b560cdba9c4ed09c619223d8890e56
plesk <pleskacv@gmail.com>
2018-05-15 12:32:51 +0200

    úprava konstruktoru rezervace

--- Rezervacnik/src/main/java/com/github/eifellovkas/Rezervacnik/ui/ControllerRezervaceNova.java
+++ Rezervacnik/src/main/java/com/github/eifellovkas/Rezervacnik/ui/ControllerRezervaceNova.java
@@ -3,10 +3,17 @@
  */
 package com.github.eifellovkas.Rezervacnik.ui;
 
+import com.github.eifellovkas.Rezervacnik.logika.Restaurace;
+import com.github.eifellovkas.Rezervacnik.logika.Rezervace;
+
 /**
  * @author plev00
  *
  */
 public class ControllerRezervaceNova {
-
+	private Restaurace restaurace;
+	
+	public void inicializace(Restaurace restaurace) {
+		this.restaurace=restaurace;
+}
 }
