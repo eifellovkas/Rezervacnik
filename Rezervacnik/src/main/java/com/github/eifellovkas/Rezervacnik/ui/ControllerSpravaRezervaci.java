728b954ec703e6e1ec5f1298b4e21bad3027b390
plesk <pleskacv@gmail.com>
2018-05-18 23:59:05 +0200

    hotova sprava rezervaci

--- Rezervacnik/src/main/java/com/github/eifellovkas/Rezervacnik/ui/ControllerSpravaRezervaci.java
+++ Rezervacnik/src/main/java/com/github/eifellovkas/Rezervacnik/ui/ControllerSpravaRezervaci.java
@@ -1,7 +1,12 @@
 
 package com.github.eifellovkas.Rezervacnik.ui;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Set;
 
 import com.github.eifellovkas.Rezervacnik.logika.Restaurace;
 import com.github.eifellovkas.Rezervacnik.logika.Rezervace;
@@ -9,12 +14,14 @@ import com.github.eifellovkas.Rezervacnik.logika.Stul;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 
 /**
@@ -36,7 +43,7 @@ public class ControllerSpravaRezervaci {
 	@FXML private DatePicker		  	datumVstup;
 	@FXML private ComboBox<Integer>  	hodinaVstup;
 	@FXML private ComboBox<Integer>  	pocetMistVstup;
-	@FXML private ComboBox<Integer>  	stulVstup;
+	@FXML private ComboBox<String>  	stulVstup;
 	@FXML private CheckBox  	kurackyVstup;
 	@FXML private TextField	jmenoVstup;
 	@FXML private Button	upravitButton;
@@ -48,17 +55,28 @@ public class ControllerSpravaRezervaci {
 		this.rezervace=rezervace;
 		this.nazev=nazev;
 		this.restaurace=restaurace;
+		pocetMistVstup.getItems().addAll(1,2,3,4,5,6,7,8,9,10);
+		hodinaVstup.getItems().addAll(11,12,13,14,15,16,17,18,19,20,21,22,23); 
 		vypisUdaje();
+		stulVstup.setDisable(true);
 	}
 	
 	public void vypisUdaje() {
-		String[] slovo = nazev.split(" -");
+		datumVypis.setText("");
+		pocetMistVypis.setText("");
+		nekurackyVypis.setText("");
+		stulVypis.setText("");
+		jmenoVypis.setText("");
+		
+		String[]slovo = nazev.split(" -");
+		
 		Stul stul = restaurace.getStul(slovo[0]);
+		
 		String den = String.valueOf(restaurace.getRezervace(nazev).getDatum().getDate());
 		String mesic = String.valueOf(restaurace.getRezervace(nazev).getDatum().getMonth()+1);
 		String rok = String.valueOf(restaurace.getRezervace(nazev).getDatum().getYear()+1900);
+		
 		datumVypis.appendText(den+"."+mesic+"."+rok);
-		System.out.println(slovo[0]);
 		pocetMistVypis.appendText(String.valueOf(stul.getPocetMist()));
 		nekurackyVypis.appendText(String.valueOf(stul.isNekuracky()));
 		stulVypis.appendText(String.valueOf(slovo[0]));
@@ -67,8 +85,104 @@ public class ControllerSpravaRezervaci {
 		}
 
 	@FXML public void vypisUpravovane(ActionEvent arg0) {
+		LocalDate date = datumVstup.getValue();
+		stulVstup.getItems().removeAll();
+		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+		Integer mista = pocetMistVstup.getValue();
+		boolean kurackyCB = kurackyVstup.isSelected();
+		Integer hodina = hodinaVstup.getValue();
+		if(hodina != null && mista != null && !date.equals(null) ) {
+			String datumFormat = date.format(format);
+
+			for(String nazev: restaurace.getSeznamStolu().keySet())
+		{
+			Stul stul = restaurace.getStul(nazev);
+			if(stul.getPocetMist() == mista && stul.isNekuracky() == kurackyCB) {
+
+				String rezervaceNazev = nazev + " - " + date + " - " + hodina;
 			
+
+				for(String nazev2: restaurace.getSeznamRezervaci().keySet()) {
+					if(!nazev2.equals(rezervaceNazev)) {
+						if(!stulVstup.getItems().contains(nazev)) {
+							
+						stulVstup.getItems().add(nazev);
+						}
+
+
+					}
+				}
+
+			}}
 		
+			
+		}
+		else {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Nedostatek údajů");
+			alert.setHeaderText(null);
+			alert.setContentText("Nejsou vyplněny všechny údaje!");
+			alert.showAndWait();
+			
+		}
+		
+			
+	if(upravitButton.getText().equals("OK")){
+		hodinaVstup.setDisable(true);
+		datumVstup.setDisable(true);
+		pocetMistVstup.setDisable(true);
+		kurackyVstup.setDisable(true);
+		stulVstup.setDisable(false);
+		upravitButton.setText("upravit");
 	}
+	else {
+		hodinaVstup.setDisable(false);
+		datumVstup.setDisable(false);
+		pocetMistVstup.setDisable(false);
+		kurackyVstup.setDisable(false);
+		stulVstup.setDisable(true);
+		upravitButton.setText("OK");
+	}
+   
 	}
 	
+	@SuppressWarnings("unused")
+	@FXML public void resetovatButton(ActionEvent arg0) {
+		
+		String stul = stulVstup.getValue();
+		String jmeno = jmenoVstup.getText();
+		if(stul != null && jmeno != null) {
+			
+			
+			datumVstup.getValue().getDayOfYear();
+			int den = datumVstup.getValue().getDayOfMonth();
+			int mesic = datumVstup.getValue().getMonthValue();
+			int rok = datumVstup.getValue().getYear();
+			Date date = new Date(rok-1900,mesic-1,den);
+			rezervace.setDatum(date);
+			rezervace.setHodina(hodinaVstup.getValue());
+			rezervace.setJmeno(jmenoVstup.getText());
+			String hodnota = stulVstup.getValue();
+			
+			
+			rezervace.setStul(restaurace.getStul(hodnota));
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Hodnota byla změněna");
+			alert.setHeaderText(null);
+			alert.setContentText("Hodnota byla změněna!!!");
+			alert.showAndWait();
+			vypisUdaje();
+			
+		}
+		
+	else  {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Nedostatek údajů");
+		alert.setHeaderText(null);
+		alert.setContentText("Nejsou vyplněny všechny údaje!");
+		alert.showAndWait();
+		
+	}
+		
+}
+}
